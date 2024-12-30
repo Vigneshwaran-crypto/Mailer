39b55893cdcffefd0cd96856c1a7be3862ca09ca
Vigneshwaran <vigneswaran@betamonks.com>
2024-12-30 12:31:23 +0530

    Issue Fixed

--- netlify/functions/dailyTask.mjs
+++ netlify/functions/dailyTask.mjs
@@ -3,29 +3,29 @@ import moment from "moment";
 
 export default async (event, context) => {
   console.log("Your scheduled task is running!");
+  try {
+    const transporter = nodemailer.createTransport({
+      // service: "gmail",
+      // auth: {
+      //   user: "Vickytata619@gmail.com",
+      //   pass: "blcn wmnt xtxz smcg",
+      // },
+      host: "betamonks.com",
+      port: 465, // Usually 587 for TLS or 465 for SSL
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: "vigneswaran@betamonks.com",
+        pass: "Pass123!@#",
+      },
+    });
 
-  const transporter = nodemailer.createTransport({
-    // service: "gmail",
-    // auth: {
-    //   user: "Vickytata619@gmail.com",
-    //   pass: "blcn wmnt xtxz smcg",
-    // },
-    host: "betamonks.com",
-    port: 465, // Usually 587 for TLS or 465 for SSL
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: "vigneswaran@betamonks.com",
-      pass: "Pass123!@#",
-    },
-  });
-
-  const today = moment().format("DD/MM/YYYY");
-  const mailOptions = {
-    from: "vigneswaran@betamonks.com",
-    to: "23msccs06@thiruthangalnadarcollege.edu.in",
-    cc: "vickytata619@gmail.com",
-    subject: `TimeSheet - Vigneshwaran - ${today}`,
-    text: `
+    const today = moment().format("DD/MM/YYYY");
+    const mailOptions = {
+      from: "vigneswaran@betamonks.com",
+      to: "23msccs06@thiruthangalnadarcollege.edu.in",
+      cc: "vickytata619@gmail.com",
+      subject: `TimeSheet - Vigneshwaran - ${today}`,
+      text: `
 Hi   sir,
 
 Kindly find my work status today,
@@ -37,15 +37,18 @@ Vigneshwaran. S
 Please follow the link :
 https://docs.google.com/spreadsheets/d/1U-MnTJjA8vzB4haTjmKfKZS4c6IT5m8nWwChqiziF4o/edit?usp=drivesdk
     `,
-  };
+    };
 
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) return console.log("Mail error:", err);
-    console.log("Mail sent Successfully:", info);
-  });
+    const info = await transporter.sendMail(mailOptions);
 
-  return {
-    statusCode: 200,
-    body: "Task completed successfully!",
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Mail Sent Successfully", info }),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Mail Failed", message: e.message }),
+    };
+  }
 };
